7e589e9ee9a0a4bfadca4aafc9c6440d265c293c
Alexander J Trelore <alextrelore@gmail.com>
2021-03-21 13:50:56 +0000

    Support for github packages

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,20 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
 	"io/fs"
 	"log"
+	"net/url"
 	"os"
 	"sort"
+	"strings"
 
 	"github.com/aybabtme/uniplot/histogram"
+	"github.com/google/go-github/v33/github"
 	"github.com/spf13/cobra"
 )
 
@@ -28,6 +32,87 @@ var rootCmd = &cobra.Command{
 func dirFilter(f fs.FileInfo) bool { return true }
 
 func run(pkg string) error {
+	if strings.HasPrefix(pkg, "github.com") {
+		return parseGithubPackage(pkg)
+	}
+
+	return parseLocalPackage(pkg)
+}
+
+func parseGithubPackage(pkg string) error {
+	u, err := url.Parse(pkg)
+	if err != nil {
+		return fmt.Errorf("parsing url: %w", err)
+	}
+
+	s := strings.Split(u.Path, "/")
+	if len(s) < 3 {
+		return fmt.Errorf("package not specified")
+	}
+	path := strings.Join(s[3:], "/")
+
+	client := github.NewClient(nil)
+
+	_, dirC, _, err := client.Repositories.GetContents(context.Background(), s[1], s[2], path, nil)
+	if err != nil {
+		return fmt.Errorf("getting package: %w", err)
+	}
+
+	publicFunctions := 0
+	fileCount := 0
+	data := []float64{}
+	imports := make(map[string]bool)
+
+	for _, f := range dirC {
+		if !strings.HasSuffix(f.GetName(), ".go") {
+			continue
+		}
+		fileCount++
+		fileC, _, _, err := client.Repositories.GetContents(context.Background(), s[1], s[2], f.GetPath(), nil)
+		if err != nil {
+			return fmt.Errorf("getting file: %w", err)
+		}
+		c, err := fileC.GetContent()
+		if err != nil {
+			return fmt.Errorf("getting file contents: %w", err)
+		}
+
+		fset := token.NewFileSet() // positions are relative to fset
+
+		fp, err := parser.ParseFile(fset, f.GetName(), c, parser.ParseComments)
+		if err != nil {
+			return fmt.Errorf("parsing file: %w", err)
+		}
+
+		publicFuncsPerFile := 0.
+		for _, d := range fp.Decls {
+			if fn, isFn := d.(*ast.FuncDecl); isFn && ast.IsExported(fn.Name.Name) {
+				publicFunctions++
+				publicFuncsPerFile++
+			}
+		}
+		data = append(data, publicFuncsPerFile)
+
+		for _, i := range fp.Imports {
+			imports[i.Path.Value] = true
+		}
+	}
+	hist := histogram.Hist(5, data)
+	err = histogram.Fprint(os.Stdout, hist, histogram.Linear(20))
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Package has %d exported function(s) across %d file(s)\n", publicFunctions, fileCount)
+
+	i := alphabetical(toSlice(imports))
+	sort.Sort(i)
+	fmt.Printf("Importing the following: %v\n", i)
+
+	return nil
+}
+
+func parseLocalPackage(pkg string) error {
 	fset := token.NewFileSet() // positions are relative to fset
 	pkgs, err := parser.ParseDir(fset, pkg, dirFilter, parser.ParseComments)
 	if err != nil {
@@ -54,7 +139,7 @@ func run(pkg string) error {
 				imports[i.Path.Value] = true
 			}
 		}
-		hist := histogram.Hist(3, data)
+		hist := histogram.Hist(5, data)
 		err := histogram.Fprint(os.Stdout, hist, histogram.Linear(20))
 		if err != nil {
 			return err
@@ -64,7 +149,7 @@ func run(pkg string) error {
 
 		i := alphabetical(toSlice(imports))
 		sort.Sort(i)
-		fmt.Println(i)
+		fmt.Printf("Importing the following: %v\n", i)
 	}
 
 	return nil
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/aybabtme/uniplot v0.0.0-20151203143629-039c559e5e7e
+	github.com/google/go-github/v33 v33.0.0
 	github.com/spf13/cobra v1.1.3
 )
